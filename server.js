3a69997498aa6c0201ab332a3d5727aa73a45778
Ana Claudia <anac.almeidasouza@gmail.com>
2021-09-04 14:49:57 -0300

    correção projeto

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 const cors = require('cors')
+app.use(cors())
 
 const db = require('./src/data/database')
 db.connect()
 
-app.use(cors())
 app.use(express.json())
 
 app.listen(3333, () => console.log('Servidor rodando na porta 3333'))
